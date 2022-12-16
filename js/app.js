c9dcb518a2b851184d499c8e1de8272eaeb128a2
tangosai <66020310+tangosai@users.noreply.github.com>
2022-12-16 23:08:50 +0530

    final
    final

--- js/app.js
+++ js/app.js
@@ -1,6 +1,42 @@
+//menu
 const hamburgerMenu = document.getElementById('toggle');
 const sliderBar = document.getElementById('menu');
-hamburgerMenu.onclick = function(){
+hamburgerMenu.onclick = function () {
 	hamburgerMenu.classList.toggle('active');
 	sliderBar.classList.toggle('active')
 }
+
+
+//swiper js
+const swiper = new Swiper('.swiper', {
+	loop: true,
+	spaceBetween: 20,
+	autoplay: {
+		delay: 2500,
+		disableOnInteraction: false,
+	},
+	effect: "coverflow",
+	grabCursor: true,
+	centeredSlides: true,
+	slidesPerView: 2,
+	coverflowEffect: {
+		rotate: 30,
+		stretch: 0,
+		depth: 300,
+		modifier: 1,
+		slideShadows: true,
+	},
+	// If we need pagination
+	pagination: {
+		el: '.swiper-pagination',
+	},
+
+	// Navigation arrows
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
+
+
+});
+
